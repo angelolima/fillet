0d9f6d63a13669846cb97ccbb5fa02cee08aa5ad
Angelo Studio <emaildostudiohttp@gmail.com>
2020-07-24 11:42:28 -0300

    v1.0.1

---
+++ src/collections/db-conn.js
@@ -0,0 +1,16 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : 3306,
+  user     : 'root',
+  password : 'tripno08',
+  database : 'sysfit'
+});
+
+connection.connect(function(err){
+  if(err) return console.log(err);
+  console.log('conectou!');
+})
+ 
+
+
